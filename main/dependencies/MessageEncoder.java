aa85941f54be8838276928affcbeba09b8c5d456
chris19928 <34878605+chris19928@users.noreply.github.com>
2018-07-22 15:10:07 -0400

    kool
    kool

--- main/dependencies/MessageEncoder.java
+++ main/dependencies/MessageEncoder.java
@@ -3,42 +3,27 @@ import java.awt.image.*;
 import java.awt.Color;
 public class MessageEncoder {
 	private String msg;
-	private Color[][] pixels;
-	private int height, width, type;
+	private BufferedImage img;
+	private int height, width;
 	public MessageEncoder(String msg, BufferedImage img) {
 		this.msg = msg;
 		this.height = img.getHeight();
 		this.width = img.getWidth();
-		this.pixels = buildArray(img);
-		this.type = img.getType();
-	}
-	//redundant to change it into color array and back, but i wrote this before i knew bufferedimages were mutable
-	public Color[][] buildArray(BufferedImage img) {
-		Color[][] smee = new Color[height][width];
-		for (int r = 0; r < height; r++)
-			for (int c = 0; c < width; c++)
-				smee[r][c] = new Color(img.getRGB(c, r));
-		return smee;
+		this.img = img;
 	}
 	public BufferedImage encode() {
-		int r = 0, c = 0, strPos = 0, lnt = msg.length();
-		while (strPos < lnt) {
-			int newGreenValue = 255 - (int)msg.charAt(strPos);
-			pixels[r][c] = new Color(pixels[r][c].getRed(), newGreenValue, pixels[r][c].getBlue());
-			strPos++;
-			c += 10;
-			if (c > pixels[0].length) {
-				r++;
-				c = 0;
-			}
-		}
-		return buildImage();
-	}
-	private BufferedImage buildImage() {
-		BufferedImage output = new BufferedImage(width, height, type);
-		for (int c = 0; c < width; c++)
-			for (int r = 0; r < height; r++)
-				output.setRGB(c, r, pixels[r][c].getRGB());
-		return output;
+		int strPos = 0, lnt = msg.length();
+		assert(lnt != 0 && lnt <= height * width);
+		for (int y = 0; y < height; y++)
+			for (int x = 0; x < width; x += 10) {
+				Color tileRGB = new Color(img.getRGB(x, y));
+				int red = tileRGB.getRed(), green = (int)msg.charAt(strPos), blue = tileRGB.getBlue();
+				Color col = new Color(red, green, blue);
+				img.setRGB(x, y, col.getRGB());
+				strPos++;
+				if (strPos >= lnt)
+					return this.img;
+			}	
+		return this.img;
 	}
 }
